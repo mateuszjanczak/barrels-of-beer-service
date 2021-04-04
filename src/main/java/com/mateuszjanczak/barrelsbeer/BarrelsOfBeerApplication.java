9484c79bf9579e9df879fb7506de4114a68c920b
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-04-04 20:47:00 +0200

    Spring init

---
+++ src/main/java/com/mateuszjanczak/barrelsbeer/BarrelsOfBeerApplication.java
@@ -0,0 +1,13 @@
+package com.mateuszjanczak.barrelsbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrelsOfBeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarrelsOfBeerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mateuszjanczak/barrelsbeer/BarrelsOfBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszjanczak.barrelsbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarrelsOfBeerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
