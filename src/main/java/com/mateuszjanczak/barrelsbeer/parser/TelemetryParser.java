a7fdf0665e5ce6e856740f445297c4f6bf1d00a0
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-04-09 13:22:18 +0200

    Hex data as input to read from barrel tap, reset for barrel temperature logs

--- src/main/java/com/mateuszjanczak/barrelsbeer/parser/TelemetryParser.java
+++ src/main/java/com/mateuszjanczak/barrelsbeer/parser/TelemetryParser.java
@@ -2,13 +2,17 @@ package com.mateuszjanczak.barrelsbeer.parser;
 
 import org.springframework.stereotype.Component;
 
+import java.math.BigInteger;
+
 @Component
 public class TelemetryParser {
     public TelemetryData parseRawData(String value) {
-        String data = value.trim().replace(" ", "");
 
-        String binaryFlowCounter = data.substring(0, 30);
-        String binaryBarrelTemperature = data.substring(46, 60);
+        String hexData = value.trim().replace(" ", "");
+        String binaryData = hexToBin(hexData);
+
+        String binaryFlowCounter = binaryData.substring(0, 30);
+        String binaryBarrelTemperature = binaryData.substring(46, 60);
 
         float flowCounter = Float.intBitsToFloat(Integer.parseInt(binaryFlowCounter, 2)) * 1000;
         float barrelTemperature = Integer.parseInt(binaryBarrelTemperature, 2) / 10.0f;
@@ -18,5 +22,9 @@ public class TelemetryParser {
         telemetryData.setBarrelTemperature(barrelTemperature);
 
         return telemetryData;
+    }
+
+    private String hexToBin(String s) {
+        return new BigInteger(s, 16).toString(2);
     }
 }
--- src/main/java/com/mateuszjanczak/barrelsbeer/service/AdminService.java
+++ src/main/java/com/mateuszjanczak/barrelsbeer/service/AdminService.java
@@ -2,20 +2,24 @@ package com.mateuszjanczak.barrelsbeer.service;
 
 import com.mateuszjanczak.barrelsbeer.domain.repository.BarrelTapLogRepository;
 import com.mateuszjanczak.barrelsbeer.domain.repository.BarrelTapRepository;
+import com.mateuszjanczak.barrelsbeer.domain.repository.BarrelTemperatureLogRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
     private final BarrelTapRepository barrelTapRepository;
     private final BarrelTapLogRepository barrelTapLogRepository;
+    private final BarrelTemperatureLogRepository barrelTemperatureLogRepository;
 
-    public AdminService(BarrelTapRepository barrelTapRepository, BarrelTapLogRepository barrelTapLogRepository) {
+    public AdminService(BarrelTapRepository barrelTapRepository, BarrelTapLogRepository barrelTapLogRepository, BarrelTemperatureLogRepository barrelTemperatureLogRepository) {
         this.barrelTapRepository = barrelTapRepository;
         this.barrelTapLogRepository = barrelTapLogRepository;
+        this.barrelTemperatureLogRepository = barrelTemperatureLogRepository;
     }
 
     public void resetDB() {
         barrelTapRepository.deleteAll();
         barrelTapLogRepository.deleteAll();
+        barrelTemperatureLogRepository.deleteAll();
     }
 }
