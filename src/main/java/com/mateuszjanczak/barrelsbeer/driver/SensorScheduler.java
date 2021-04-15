45ff737ca2f7c8fd75ce0ea2eb11f249c95819d5
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-04-16 01:36:39 +0200

    Fixed read data from sensor

--- src/main/java/com/mateuszjanczak/barrelsbeer/driver/SensorScheduler.java
+++ src/main/java/com/mateuszjanczak/barrelsbeer/driver/SensorScheduler.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -33,11 +34,14 @@ public class SensorScheduler {
     public void readData() {
         for(BarrelTap barrelTap: barrelTapList) {
             int barrelTapId = barrelTap.getBarrelTapId();
-            ResponseEntity<SensorData> optionalSensorData = restTemplate.exchange("http://192.168.136.110//iolinkmaster/port%5B"+ barrelTapId +"%5D/iolinkdevice/pdin/getdata", HttpMethod.GET, HttpEntity.EMPTY, SensorData.class);
+            String url = "http://192.168.136.110//iolinkmaster/port["+ barrelTapId +"]/iolinkdevice/pdin/getdata";
+            ResponseEntity<SensorData> optionalSensorData = restTemplate.exchange(url, HttpMethod.GET, HttpEntity.EMPTY, SensorData.class);
             if(optionalSensorData.getStatusCode().is2xxSuccessful()) {
                 String hex = Objects.requireNonNull(optionalSensorData.getBody()).getData().getValue();
                 barrelTapService.hitBarrelTap(barrelTapId, hex);
+                System.out.println("TAP ID: " + barrelTapId + ", URL: " + url + ", HEX: " + hex);
             }
         }
+        System.out.println("UPDATE DATE: " + new Date());
     }
 }
